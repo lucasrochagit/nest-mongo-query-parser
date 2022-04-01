d980fe9b6f7ba65cd617e216bef8c26290ba3b67
Lucas Rocha <lucascosmorocha@gmail.com>
2022-04-01 11:25:15 -0300

    fix: adjust date validators that does not accepts october month

--- src/lib/utils/string.validator.ts
+++ src/lib/utils/string.validator.ts
@@ -8,13 +8,13 @@ export class StringValidator {
   }
 
   public static isISODate(str: string): boolean {
-    return /^(19[6-9]\d|2\d\d\d)-(0[1-9]|1[1-2])-(0[1-9]|1\d|2\d|3[0-1])$/.test(
+    return /^(19[6-9]\d|2\d\d\d)-(0[1-9]|1[0-2])-(0[1-9]|1\d|2\d|3[0-1])$/.test(
       str
     );
   }
 
   public static isISODateTime(str: string): boolean {
-    return /^(19[6-9]\d|2\d\d\d)-(0[1-9]|1[1-2])-(0[1-9]|1\d|2\d|3[0-1])T(0\d|1\d|2[0-3]):([0-5]\d):([0-5]\d).(\d\d\d)(|Z)$/.test(
+    return /^(19[6-9]\d|2\d\d\d)-(0[1-9]|1[0-2])-(0[1-9]|1\d|2\d|3[0-1])T(0\d|1\d|2[0-3]):([0-5]\d):([0-5]\d).(\d\d\d)(|Z)$/.test(
       str
     );
   }
