ee543d13c13e69fc7e5bdacf90f3e09d3e7fbb68
Lucas Rocha <lucascosmorocha@gmail.com>
2023-02-06 16:42:50 -0300

    feat: add methods to add query params in code easily

--- src/lib/model/mongo.query.model.ts
+++ src/lib/model/mongo.query.model.ts
@@ -5,6 +5,40 @@ export class MongoQueryModel {
   select!: QueryObjectModel;
   filter!: QueryObjectModel;
   populate!: QueryObjectModel | QueryObjectModel[];
+
+  addSort(sort: QueryObjectModel): void {
+    this.sort = {...this.sort, ...sort}
+  }
+
+  addSelect(select: QueryObjectModel): void {
+    this.select = {...this.select, ...select}
+  }
+
+  addFilter(filter: QueryObjectModel): void {
+    this.filter = {...this.filter, ...filter}
+  }
+
+  addPopulate(populate: QueryObjectModel | QueryObjectModel[]): void {
+    if (this.populate instanceof Array) {
+      if (populate instanceof Array) {
+        this.populate = [...this.populate, ...populate];
+      } else {
+        this.populate.push(populate);
+      }
+    } else {
+      if (populate instanceof Array) {
+        populate.push(this.populate);
+        this.populate = populate;
+      }
+      else {
+        if (populate.path === this.populate.path) {
+          this.populate = populate;
+        } else {
+          this.populate = [this.populate, populate];
+        }
+      }
+    }
+  }
 }
 
 export class QueryObjectModel {
